683e191ba07ded343a92745cd2b14244ab393906
Danijel Korzinek <danijel@pja.edu.pl>
2016-08-25 08:36:52 +0200

    Fixed data prep script. Added BOM removal. Added md5sum checking...

--- local_clarin/clarin_pl_data_prep.sh
+++ local_clarin/clarin_pl_data_prep.sh
@@ -9,6 +9,14 @@ AUDIO_DL_PATH=./data
 	if [ ! -f audio.tar.gz ] ; then
 		echo "Downloading audio from the Clarin-pl website (~4.6GB)..."
 		curl -O http://mowa.clarin-pl.eu/korpusy/audio.tar.gz
+	else
+		echo "File already downloaded! Checking if download is consistent..."
+		curl -O http://mowa.clarin-pl.eu/korpusy/audio.md5sum
+		if ! md5sum -c audio.md5sum ; then
+			echo "Download doesn't match the one on the server! "
+			echo "Erase the audio.tar.gz file (and audio folder) and run this script again!"
+			exit -1
+		fi
 	fi
 
 	if [ ! -d audio	] ; then
@@ -16,7 +24,7 @@ AUDIO_DL_PATH=./data
 		tar xf audio.tar.gz
 	else
 		echo "Files already extracted?"
-		echo "Remove the audio dir to extract them egain..."
+		echo "Remove the audio dir to extract them again..."
 	fi
 )
 
@@ -24,5 +32,10 @@ AUDIO_DL_PATH=./data
 echo Generating file lists using proper paths...
 python local_clarin/generate_lists.py $AUDIO_DL_PATH/audio data
 
+echo Removing BOMs from data...
+sed -i 's/\xef\xbb\xbf//g' data/test/text
+sed -i 's/\xef\xbb\xbf//g' data/train/text
+
+echo Generating spk2utt...
 utils/utt2spk_to_spk2utt.pl data/train/utt2spk > data/train/spk2utt
 utils/utt2spk_to_spk2utt.pl data/test/utt2spk > data/test/spk2utt
